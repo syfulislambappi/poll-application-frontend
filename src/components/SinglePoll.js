9ee6bf5894e961e0a2b5431a13efd58226327bc4
Syful Islam <48632407+syfulislambappi@users.noreply.github.com>
2022-10-25 21:15:48 +0600

    added update features

--- src/components/SinglePoll.js
+++ src/components/SinglePoll.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { redirect, useParams } from "react-router-dom";
 import {
+  Button,
   Card,
   CardBody,
   CardText,
@@ -15,6 +16,9 @@ import getSingleData from "../utils/getSingleData";
 const SinglePoll = () => {
   const { pollId } = useParams();
   const [poll, setPoll] = useState({});
+  const [change, setChange] = useState(0);
+  const [voteId, setVoteId] = useState("");
+
   useEffect(() => {
     const uri = `http://localhost:4000/api/v1/poll/${pollId}`;
 
@@ -22,10 +26,41 @@ const SinglePoll = () => {
       const data = await getSingleData(uri);
       setPoll(data[0]);
     })();
-  }, [pollId]);
-  console.log(poll);
+  }, [pollId, change]);
+
+  const radioHandler = (e) => {
+    setVoteId(e.target.value);
+  };
+
+  const updateVote = () => {
+    // Update poll values
+    poll.totalVote = poll.totalVote + 1;
+    const vote = poll?.options?.find((value) => value._id === voteId);
+    vote.vote = vote.vote + 1;
+
+    // Update information
+    const uri = `http://localhost:4000/api/v1/poll/${pollId}`;
+    const updateData = { pollId, voteId };
+
+    // Update request
+    (async () => {
+      await fetch(uri, {
+        method: "PATCH",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(updateData),
+      });
+    })();
+    setChange((prevState) => prevState + 1);
+  };
+
   return (
-    <Container>
+    <Container
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Card
         body
         className="my-2"
@@ -35,15 +70,27 @@ const SinglePoll = () => {
       >
         <CardTitle tag="h5">{poll?.title}</CardTitle>
         <CardText>{poll?.description}</CardText>
+        <CardText>{poll?.totalVote}</CardText>
         <CardBody>
           <h6>Options</h6>
           {poll?.options?.map((value, index) => (
-            <FormGroup key={index} check>
-              <Input type="radio" name="option" />
-              <Label check>{value.name}</Label>
+            <FormGroup
+              key={index}
+              check
+              style={{ display: "flex", justifyContent: "space-between" }}
+              onChange={radioHandler}
+            >
+              <div>
+                <Input type="radio" name="option" value={value._id} />
+                <Label check>{value.name}</Label>
+              </div>
+              <span>{((value.vote / poll?.totalVote) * 100).toFixed(2)}%</span>
             </FormGroup>
           ))}
         </CardBody>
+        <Button color="primary" onClick={updateVote}>
+          Vote
+        </Button>
       </Card>
     </Container>
   );
